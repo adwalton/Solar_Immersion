7dd7d68988da10e419076830a5e14e0a48714695
Andrew Walton <walton.andrew1@gmail.com>
2016-02-21 20:01:28 +0000

    Adjusted RAG thresholds. Added piezo sounder

--- Solar_Immersion.ino
+++ Solar_Immersion.ino
@@ -1,20 +1,14 @@
+// Solar Immersion 
 // 
-// 2014-04-15 - increased max PWM output level 'cos volts on immersion were not reaching 230V 
-// 2014-03-08
-// Changes:
-// Increased remote transmit time to 1.5 seconds to try to improve reliability
-// Corrected error stopping Traffic Lights working in sequence
-// Added minPWMOn parameter to enable PWM output to jump straight from it's absolutely OFF value (minPWMOut) to a level that delivers power to the heater as soon as solar export is detected
-// Confirmed that sockets come on at Amber level and off when solar import detected
-//
 // Based on Kemo PWM mains controller and Grid Meter LED input signals only.
 //
-// Now includes two relays for driving remote socket switch on via Byron controller
+// Includes two relays for driving remote socket switch on via Byron controller
 //
 // 1/10/2014 - Removed Bargraph and CT Code. Added "meterTest" section to test if Grid Meter LED is really on. It does this by checking if it stays on for > meterLEDOnTime
 // test change
 //
 boolean onboardLED; // define flag for onboard LED status.Onboard LED used to give a 'heartbeat' that toggles each run of main loo
+const int piezoPin = 8; // Pin for Piezo Sounder
 const int meterLEDPin = 30; //Digital pin used to monitor Grid Meter LED pulses
 const int PWMOutPin = 12; //Immersion heater controller output (PWM)
 const int remoteOnPin = 40; //Digital output to turn remote sockets ON
@@ -29,11 +23,11 @@ unsigned long stepTime = 3000; //Duration of time between incremental PWM adjust
 unsigned long lastLoopStartTime = millis(); // Used to store start time of last loop 
 const int maxPWMOut = 240; //Used to scale PWM output - set to just over 90% of Arduino range (90% is supposed to give Kemo full ON
 const int minPWMOut = 0; //Used to set minimum PWM level - Kemo OFF at less than 10% mark-space ratio
-const int minPWMOn = 77; //PWM value that represents lowest point where power actually starts going to immersion heater (minPWMOut is set at a level to ensure heater is definitely OFF when it should be)
-const int redLevel = 120; //PWM level to turn on red LED
-const int amberLevel = 160; //PWM level to turn on amber LED
-const int greenLevel = 200; //PWM level to turn on green LED
-const int remotePushTime = 1500; //Time to "hold down button on Remote)
+const int minPWMOn = 80; //PWM value that represents lowest point where power actually starts going to immersion heater (minPWMOut is set at a level to ensure heater is definitely OFF when it should be)
+const int redLevel = 100; //PWM level to turn on red LED
+const int amberLevel = 120; //PWM level to turn on amber LED
+const int greenLevel = 140; //PWM level to turn on green LED
+const int remotePushTime = 3000; //Time to "hold down button on Remote)
 const int PWMDecrementFactor = 11; //used to provide non-linear, accelerated reduction in PWM Output 
 int flashCount = 0; // Used in flash function to count down 
 boolean meterLEDOn = false; //Flag: 0 if Grid Meter LED off, 1 if on
@@ -42,13 +36,13 @@ const int meterLEDOnTime = 500; //If LED has been on for this time (ms), we cons
 int PWMOut = minPWMOut; //PWM output to Kemo; set to min to be sure power is not immediately applied
 int latestMaxPWM = minPWMOn; // Initialise maxPWM out variable
 //
-int socketsOn = 0; //variable to store the state of the remote sockets - set to OFF initially
+boolean socketsOn = false; //variable to store the state of the remote sockets - set to false initially
 unsigned long minSocketOnTime = 120000; // Turning sockets off is not allowed until they've been on for this long (mS)
 unsigned long socketLastOnTime; // Used to store the millis() time when sockets turned on
 unsigned long minSocketOffTime = 120000; // Turning sockets on is not allowed until they've been off for this long (mS)
 unsigned long socketLastOffTime; // Used to store the millis() time when sockets turned off
 //
-//  Set up for calculating avaerage of recent PWM maximum values
+//  Set up for calculating average of recent PWM maximum values
 //
 const int nRecentPWMs = 4; //Number of recent energy values to store. MUST BE EVEN.
 int recentPWMs[nRecentPWMs]; // Create array to store PWM readings
@@ -81,51 +75,11 @@ void setup()
   {
     recentPWMs[i] = minPWMOn;
   }
-Serial.println("Solar_Immersion 13/02/2016");
+Serial.println("Solar_Immersion 14/02/2016");
 }  
 void loop()
 {
-  if((millis()-lastLoopStartTime) > stepTime)
-  {
-    lastLoopStartTime = millis(); // Reset stored main loop start time
-// Toggle Onboard LED each loop
-//
-    onboardLED = !onboardLED;
-    digitalWrite(13,onboardLED);
-// 
-// print the results to the serial monitor: in CSV Format for cut and paste into Excel
-// Sequence is: PWMOut, meterLEDOn, socketsOn
-//      
-  Serial.print("PWMOut = ");
-  Serial.print(PWMOut);
-  Serial.print(", meterLEDOn = ");
-  Serial.print(meterLEDOn);
-  Serial.print(", newOnCycle = ");
-  Serial.print(newOnCycle);
-  Serial.print(", latestMaxPWM = ");
-  Serial.print(latestMaxPWM);
-  Serial.print(", socketsOn = ");
-  Serial.print(socketsOn);
-  if (PWMOut > redLevel)
-  {
-    Serial.print(" ");
-    Serial.print("R");
-  }
-  else
-  {
-    Serial.print("  ");    
-  }
-if (PWMOut > amberLevel)
-  {
-    Serial.print("A");
-  }
-if (PWMOut > greenLevel)
-  {
-    Serial.print("G");
-  }
-  Serial.println(" ");
- //
- // Set meterLEDOn Flag
+ // Set meterLEDOn Flag (Outside 'stepTime' Loop)
  //
     if (digitalRead(meterLEDPin)) 
     {
@@ -143,8 +97,59 @@ if (PWMOut > greenLevel)
     {
       meterLEDOn = false;
     }
+// Immediately turn off Immersion if power being exported
+//
+  if (meterLEDOn)
+  {
+  // If LED on, do nothing for now, pending timed 'stepTime' loop  
+  }
+  else
+  {    
+    PWMOut = minPWMOut;
+    newOnCycle = true;
+    turnSocketsOff();
+    analogWrite(PWMOutPin, PWMOut); //write PWM signal to mains controller
+  }
  //
- //
+ // Start of Loop that Only Runs Every 'stepTime' mS 
+  if((millis()-lastLoopStartTime) > stepTime)
+  {
+    lastLoopStartTime = millis(); // Reset stored main loop start time
+// Toggle Onboard LED each loop
+//
+    onboardLED = !onboardLED;
+    digitalWrite(13,onboardLED);
+// 
+// Print status to the serial monitor
+//      
+  Serial.print("PWMOut = ");
+  Serial.print(PWMOut);
+  Serial.print(", meterLEDOn = ");
+  Serial.print(meterLEDOn);
+  Serial.print(", newOnCycle = ");
+  Serial.print(newOnCycle);
+  Serial.print(", latestMaxPWM = ");
+  Serial.print(latestMaxPWM);
+  Serial.print(", socketsOn = ");
+  Serial.print(socketsOn);
+  if (latestMaxPWM > redLevel)
+  {
+    Serial.print(" ");
+    Serial.print("R");
+  }
+  else
+  {
+    Serial.print("  ");    
+  }
+  if (latestMaxPWM > amberLevel)
+  {
+    Serial.print("A");
+  }
+  if (latestMaxPWM > greenLevel)
+  {
+    Serial.print("G");
+  }
+    Serial.println(" ");
  //
  if (meterLEDOn)
     {
@@ -156,7 +161,12 @@ if (PWMOut > greenLevel)
         {
           if(newOnCycle)
             {
+              tone(piezoPin, 1400, 30); // Sound piezo at start of each on cycle
               PWMOut = latestMaxPWM - PWMDecrement;
+              if (PWMOut < minPWMOn)
+              {
+                PWMOut = minPWMOn;
+              }
             }
           else
             {
@@ -171,6 +181,10 @@ if (PWMOut > greenLevel)
             }
         }
           latestMaxPWM = PWMOut;
+          if (latestMaxPWM < minPWMOn)
+            {
+              latestMaxPWM = minPWMOn;
+            }
           newOnCycle = false;
     }
   else
@@ -181,93 +195,93 @@ if (PWMOut > greenLevel)
     }
   analogWrite(PWMOutPin, PWMOut); //write PWM signal to mains controller
   //
-  //Set LED 'Traffic Lights' and Turn on Remote Sockets, if appropriate
-  if(PWMOut > redLevel)
-   {
-     digitalWrite(redLEDPin, HIGH);
-   }
-  else
-   {
-    digitalWrite(redLEDPin, LOW);
-   }
-  if(PWMOut > amberLevel) // At PWM level greater than amberLevel turn on amber LED and fire Sockets
-   {
-     digitalWrite(amberLEDPin, HIGH);
-     turnSocketsOn();
-   }
-   else
-   {
-     digitalWrite(amberLEDPin, LOW);
-   }
-   if(PWMOut > greenLevel) 
-   {
-     digitalWrite(greenLEDPin, HIGH);
-   }
-   else
-   {
-     digitalWrite(greenLEDPin, LOW);
-   }
-//  flashPWM();
-  delay(stepTime);
-  }
+  //Set RAG LED 'Traffic Lights' and Turn on Remote Sockets, if appropriate
+  //
+    if(latestMaxPWM > redLevel)
+    {
+      digitalWrite(redLEDPin, HIGH);
+    }
+    else
+    {
+      digitalWrite(redLEDPin, LOW);
+    }
+    if(latestMaxPWM > amberLevel) // At PWM level greater than amberLevel turn on amber LED and fire Sockets
+    {
+      digitalWrite(amberLEDPin, HIGH);
+      turnSocketsOn();
+    }
+    else
+    {
+      digitalWrite(amberLEDPin, LOW);
+    }
+    if(latestMaxPWM > greenLevel) 
+    {
+      digitalWrite(greenLEDPin, HIGH);
+    }
+    else
+    {
+      digitalWrite(greenLEDPin, LOW);
+    }
+// END of RAG LED Update   
+//
+} // End of main 'stepTime' Loop
   else
   {
     // Come here to do nothing, if stepTime has not yet elapsed    
   }
-}
-//
-//End of main loop
+}  // END of MAIN LOOP
 //
 //FUNCTIONS
 //
-void turnSocketsOn() //Checks if already ON and if not sends pulse to remote
+void turnSocketsOn() //Checks if already ON and if not sends pulse to remote transmitter
 {
-  if(socketsOn > 0) 
+  if (socketsOn) 
   {
-    socketLastOnTime = millis(); // update the time that sockets were last demanded to be on
+//    Already ON so take no action 
   }
   else
   {
-    if((millis() - socketLastOffTime) > minSocketOffTime)
+    if ((millis() - socketLastOffTime) > minSocketOffTime)
     {
-    digitalWrite(remoteOnPin, HIGH);
-    delay(remotePushTime);
-    digitalWrite(remoteOnPin, LOW);
-    socketsOn = 1;
-    socketLastOnTime = millis(); // store the time that sockets were turned on
+      tone(piezoPin, 500, 300);
+      delay(300);
+      tone(piezoPin, 600, 300);
+      delay(300);
+      tone(piezoPin, 1400, 300);
+      delay(300);
+      digitalWrite(remoteOnPin, HIGH);
+      delay(remotePushTime);
+      digitalWrite(remoteOnPin, LOW);
+      socketsOn = true;
+      socketLastOnTime = millis(); //Store time of last switch-on
     }
   }
+  socketLastOnTime = millis(); // update the time that sockets were last demanded to be ON
 }
 //
 void turnSocketsOff() //Checks aleady ON and if not sends pulse to remote
 {
-  if(socketsOn < 1) 
-  {
-    socketLastOffTime = millis(); // update the time that sockets were last demanded to be off
-  }
-  else
+  if (socketsOn) 
   {
     if ((millis() - socketLastOnTime) > minSocketOnTime)
     {
-    digitalWrite(remoteOffPin, HIGH);
-    delay(remotePushTime);
-    digitalWrite(remoteOffPin, LOW);
-    socketsOn = 0;
-    socketLastOffTime = millis(); // store the time that sockets were turned off
+      tone(piezoPin, 1400, 300);
+      delay(300);
+      tone(piezoPin, 600, 300);
+      delay(300);
+      tone(piezoPin, 500, 300);
+      delay(300);
+      digitalWrite(remoteOffPin, HIGH);
+      delay(remotePushTime);
+      digitalWrite(remoteOffPin, LOW);
+      socketsOn = false;
     }
   }
+  else
+  {
+    //  Already OFF so take no action
+  }
+  socketLastOffTime = millis(); // update the time that sockets were last demanded to be off
 }
-//
-//void flashPWM()
-//{
-// flashCount = PWMOut + 1;
-// while (flashCount > minPWMOut)
-//  {
-//    digitalWrite(13, HIGH);
-//    delay(150);
-//    digitalWrite(13,LOW);
-//    delay(100);
-//    flashCount = flashCount - 10;
-//  }
-//}
+
 
